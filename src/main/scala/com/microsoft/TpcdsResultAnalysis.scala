59174a819c21c375a510ee474be03f089e105d71
Warren Zhu <zhonzh@microsoft.com>
2019-12-23 14:13:41 -0800

    Implement benchmark result analysis to parse json into csv

---
+++ src/main/scala/com/microsoft/TpcdsResultAnalysis.scala
@@ -0,0 +1,59 @@
+package com.microsoft
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+/**
+ * Usage: TpcdsResultAnalysis [rawResult] [csvResult]
+ */
+object TpcdsResultAnalysis {
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      printUsage()
+      System.exit(1)
+    }
+
+    val spark = SparkSession
+      .builder
+      .appName("TPC-DS Benchmark Result analysis")
+      .master("local")
+      .config("spark.sql.shuffle.partitions", 1)
+      .getOrCreate()
+
+    val rawResult = args(0)
+    val csvResult = args(1)
+
+    analysis(spark, rawResult, csvResult)
+
+    spark.stop()
+  }
+
+  private def analysis(spark: SparkSession, rawResult: String, csvResult: String) = {
+
+    val result = spark.read.json(rawResult)
+    val results = result.select(explode(result("results")), col("version")).toDF("results", "version")
+
+    results
+      .withColumn("Name", col("results.name"))
+      .withColumn("Time", (col("results.parsingTime") + col("results.analysisTime") + col("results.optimizationTime") + col("results.planningTime") + col("results.executionTime")) / 1000)
+      .drop(col("results"))
+      .groupBy("Name")
+      .pivot("version")
+      .sum("Time")
+      .coalesce(1)
+      .write
+      .format("csv")
+      .option("header", "true")
+      .mode("append")
+      .save(csvResult)
+  }
+
+  private def printUsage(): Unit = {
+    val usage = """ TpcdsResultAnalysis
+                  |Usage: rawResult csvResult
+                  |rawResult - (string) Directory contains tpcds result in json format
+                  |csvResult - (string) Directory for writing benchmark result in csv"""
+
+    println(usage)
+  }
+}
