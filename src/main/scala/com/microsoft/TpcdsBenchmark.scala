e4bc1c7bf22f3984b3d0156847152d9430a8f7ac
Warren Zhu <zhonzh@microsoft.com>
2019-12-27 19:13:05 -0800

    Support passing database name when create external table for analyzing

--- src/main/scala/com/microsoft/TpcdsBenchmark.scala
+++ src/main/scala/com/microsoft/TpcdsBenchmark.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SQLContext
 
 /**
- * Usage: TpcdsBenchmark [dataDir] [result] [queries] [iterations]
+ * Usage: TpcdsBenchmark [dataDir] [result] [databaseName] [queries] [iterations]
  */
 object TpcdsBenchmark {
   def main(args: Array[String]) {
@@ -20,25 +20,31 @@ object TpcdsBenchmark {
 
     val dataDir = args(0)
     val resultDir = args(1)
-    val queries = if(args.length > 2) args(2) else ""
-    val iterations = if(args.length > 3) args(3) else "1"
+    val databaseName = if(args.length > 2) args(2) else "tpcds"
+    val queries = if(args.length > 3) args(3) else ""
+    val iterations = if(args.length > 4) args(4) else "1"
     val sqlContext = new SQLContext(spark.sparkContext)
+
     // Run:
     val tables = new TPCDSTables(sqlContext,
       scaleFactor = "1",
       useDoubleForDecimal = false, // true to replace DecimalType with DoubleType
       useStringForDate = false) // true to replace DateType with StringType
 
-    // Create the specified database
-    // sqlContext.sql(s"create database $databaseName")
-    // Create metastore tables in a specified database for your data.
-    // Once tables are created, the current database will be switched to the specified database.
-    //tables.createExternalTables(dataDir, "parquet", databaseName, overwrite = true, discoverPartitions = true)
-    // Create temporary tables to avoid concurrent benchmark run affecting each other
-    tables.createTemporaryTables(dataDir, "parquet")
+    if(!spark.catalog.databaseExists(databaseName)) {
+      // Create the specified database
+      sqlContext.sql(s"create database $databaseName")
+      // Create metastore tables in a specified database for your data.
+      // Once tables are created, the current database will be switched to the specified database.
+      tables.createExternalTables(dataDir, "parquet", databaseName, overwrite = true, discoverPartitions = true)
+      // Create temporary tables to avoid concurrent benchmark run affecting each other
+      // tables.createTemporaryTables(dataDir, "parquet")
 
-    // For CBO only, gather statistics on all columns:
-    // tables.analyzeTables(databaseName, analyzeColumns = true)
+      // For CBO only, gather statistics on all columns:
+      tables.analyzeTables(databaseName, analyzeColumns = true)
+    } else {
+      println(s"Database $databaseName already existed")
+    }
 
     val tpcds = new TPCDS (sqlContext = sqlContext)
 
@@ -63,6 +69,7 @@ object TpcdsBenchmark {
                   |Usage: dataDir resultDir
                   |dataDir - (string) Directory contains tpcds dataset in parquet format
                   |resultDir - (string) Directory for writing benchmark result
+                  |databaseName - (string) database name
                   |queries - (string) Queries to run separated by comma, such as 'q4,q5'. Default all
                   |iterations - (int) The number of iterations for each query. Default 1"""
 
